36f50ec98df96e8eece9af91762834d389a6b587
John Yorke <yorkejohn02@gmail.com>
2023-12-03 16:15:45 -0400

    (untracked) feat #use command line options

--- client/src/main/java/com/larix/client/Main.java
+++ client/src/main/java/com/larix/client/Main.java
@@ -1,45 +1,49 @@
 package com.larix.client;
 
 import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialOceanicIJTheme;
-import com.formdev.flatlaf.util.SystemInfo;
 import com.larix.client.view.EspressoClient;
+import org.apache.commons.cli.*;
 
 import javax.swing.*;
 
+import static com.formdev.flatlaf.util.SystemInfo.isMacOS;
 import static java.lang.Integer.parseInt;
-import static java.lang.System.exit;
-import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
-import static javax.swing.JOptionPane.showConfirmDialog;
+import static java.lang.System.err;
+import static java.lang.System.setProperty;
 
 public class Main {
 
     public static void main(final String[] args) {
-        if (SystemInfo.isMacOS) {
-            System.setProperty("apple.awt.application.name", "Espresso Chat");
-            System.setProperty("apple.awt.application.appearance", "system");
+        // set macOS properties for look and feel
+        if (isMacOS) {
+            setProperty("apple.awt.application.name", "Espresso Chat");
+            setProperty("apple.awt.application.appearance", "system");
         }
 
-        SwingUtilities.invokeLater(Main::startApp);
+        // command line options
+        final Options options = new Options();
+        options.addOption(Option.builder("u").longOpt("username").hasArg().argName("username").desc("Username").required().build());
+        options.addOption(Option.builder("h").longOpt("host").hasArg().argName("host").desc("Server Hostname").required().build());
+        options.addOption(Option.builder("p").longOpt("port").hasArg().argName("port").desc("Server Port").required().build());
+
+        try {
+            // parse options
+            final CommandLine cmd = new DefaultParser().parse(options, args);
+            final String username = cmd.getOptionValue("u");
+            final String host = cmd.getOptionValue("h");
+            final String port = cmd.getOptionValue("p");
+
+            SwingUtilities.invokeLater(() -> startApp(username, host, port));
+        } catch (final ParseException e) {
+            err.println(e.getMessage());
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("Espresso Chat Client", options);
+        }
     }
 
-    private static void startApp() {
+    private static void startApp(final String username, final String host, final String port) {
         FlatMaterialOceanicIJTheme.setup();
-
-        final JTextField username = new JTextField(25);
-        final JTextField host = new JTextField(25);
-        final JTextField port = new JTextField(25);
-
-        final Object[] fields = {"Username", username, "Host", host, "Port", port};
-        //final JFrame dialogRoot = new JFrame();
-        //dialogRoot.getRootPane().putClientProperty("apple.awt.transparentTitleBar", true);
-
-        final int option = showConfirmDialog(null, fields, "Espresso Chat", OK_CANCEL_OPTION);
-
-        if (option == JOptionPane.OK_OPTION) {
-            new EspressoClient(username.getText(), host.getText(), parseInt(port.getText())).start();
-        } else {
-            exit(0);
-        }
+        new EspressoClient(username, host, parseInt(port)).start();
     }
 
 }
--- server/src/main/java/com/larix/server/Main.java
+++ server/src/main/java/com/larix/server/Main.java
@@ -1,7 +1,27 @@
 package com.larix.server;
 
+import org.apache.commons.cli.*;
+
+import javax.swing.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.err;
+
 public class Main {
     public static void main(final String[] args) {
-        new EspressoServer(4123).start();
+        // command line options
+        final Options options = new Options();
+        options.addOption(Option.builder("p").longOpt("port").hasArg().argName("port").desc("Server Port").required().build());
+
+        try {
+            // parse options
+            final CommandLine cmd = new DefaultParser().parse(options, args);
+            final String port = cmd.getOptionValue("p");
+            new EspressoServer(parseInt(port)).start();
+        } catch (final ParseException e) {
+            err.println(e.getMessage());
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("Espresso Chat Server", options);
+        }
     }
 }
